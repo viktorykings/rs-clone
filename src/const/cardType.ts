12572b0cd3ae8995842ae36ed717404e3c28c842
Andkhiz <andkhiz@gmail.com>
2023-01-31 17:44:43 +0200

    docs: create array of type cards

---
+++ src/const/cardType.ts
@@ -0,0 +1,19 @@
+import ICardType from '../interface/ICardType';
+
+const cardType: ICardType [] = [
+  { name: 'Взрывной котенок', description: '' },
+  { name: 'Обезвредить', description: '' },
+  { name: 'Нет', description: '' },
+  { name: 'Атаковать', description: '' },
+  { name: 'Пропустить', description: '' },
+  { name: 'Одолжение', description: '' },
+  { name: 'Перемешать', description: '' },
+  { name: 'Заглянуть в будущее', description: '' },
+  { name: 'Котенок', description: '' },
+  { name: 'Котенок', description: '' },
+  { name: 'Котенок', description: '' },
+  { name: 'Котенок', description: '' },
+  { name: 'Котенок', description: '' },
+];
+
+export default cardType;
