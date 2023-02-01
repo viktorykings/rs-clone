eb5992670e37908dca171b7bd5c6d13cf5e0ce1e
viktorykings <kingsviktory@gmail.com>
2023-02-01 12:17:00 +0300

    feat: add cards images links, createCard attribute for links

--- src/components/pages/DeskPage.tsx
+++ src/components/pages/DeskPage.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Player from './Players';
+import card from '../../assets/cards/beardcat.png';
+import card2 from '../../assets/cards/attack1.png';
+import card3 from '../../assets/cards/attack2.png';
 
 export default function DeskPage(): JSX.Element {
   return (
@@ -25,6 +28,9 @@ export default function DeskPage(): JSX.Element {
           <p>card</p>
           <p>card</p>
           <p>card</p>
+          <img src={card} alt="" />
+          <img src={card2} alt="" />
+          <img src={card3} alt="" />
         </div>
       </div>
     </main>
