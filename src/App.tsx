7cc591dd1115eaa57a854fb2fb71228f31056514
Eugene Mikhalionok <eugenemikhalionok@gmail.com>
2023-02-10 22:14:50 +0300

    refactor: returned DeskPage in App function

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.scss';
 import './scss/gameSettings.scss';
-// import DeskPage from './components/pages/DeskPage';
-import GameSettings from './components/pages/GameSettings/GameSettings';
+import DeskPage from './components/pages/DeskPage';
+// import GameSettings from './components/pages/GameSettings/GameSettings';
 
 function App(): JSX.Element {
   return (
     <div className="App">
-      <GameSettings />
+      <DeskPage />
+      {/* <GameSettings /> */}
     </div>
   );
 }
