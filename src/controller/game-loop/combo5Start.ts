7505a015cdc9375b221c6f1758498c9ea0f98e69
Andkhiz <andkhiz@gmail.com>
2023-02-11 10:27:57 +0200

    fix: combo5 give cards into rebound deck

--- src/controller/game-loop/combo5Start.ts
+++ src/controller/game-loop/combo5Start.ts
@@ -2,6 +2,7 @@ import IGame from '../../interface/IGame';
 
 function combo5Start(game: IGame): IGame {
   const myGame = { ...game };
+  myGame.reboundDeck.push(...myGame.showCards.splice(0));
   myGame.gameState.functionState = 'waitCombo5';
   myGame.gameState.message = 'Выберите любую карту из отбоя!';
   myGame.gameState.timeLeft = myGame.gameState.timeNeed;
