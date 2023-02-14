3611913fd41be83efe97a12162a3b0b58f868b93
Andkhiz <andkhiz@gmail.com>
2023-02-14 10:47:49 +0200

    refactor: change time to look cards to bot

--- src/controller/game-loop/subevent/getPause.ts
+++ src/controller/game-loop/subevent/getPause.ts
@@ -11,6 +11,7 @@ function getPause(isBot: boolean, functionState: TFunctionState): number {
   let time = isBot ? botWaitTurn : playerWaitTurn;
   if (functionState === 'waitAnserTurn') time = isBot ? botWaitAnswer : playerWaitTurn;
   if (functionState === 'waitNeutralize') time = isBot ? waitEndMove : playerWaitTurn;
+  if (functionState === 'waitPlayerLook') time = isBot ? waitEndMove : playerWaitTurn;
   return functionState === 'waitEndMove' ? waitEndMove : time;
 }
 
