8970b816e8ad574edc217bd8ddac9b7bc9f3cc9d
viktorykings <kingsviktory@gmail.com>
2023-02-19 15:26:29 +0300

    fix: hide cards from player when bot moves see future

--- src/components/pages/Players.tsx
+++ src/components/pages/Players.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
-// import ICard from '../../interface/ICard';
-// import IPlayer from '../../interface/IPlayer';
 
 interface Iplayer {
-  // id: number,
   name: string,
   className: string,
   link: string,
-  // deck: ICard[],
-  // isBot: boolean,
-  // url?:string
 }
-// const cardBack = 'cards/back.png';
 
 export default function Player({
   name, className, link,
@@ -21,16 +14,8 @@ export default function Player({
     <div className={`player ${className}`}>
       <div className="info">
         <p>{name}</p>
-        {/* <button type="button">Take card!</button> */}
       </div>
       <img src={link} alt={name} />
-      {/* {isBot
-        ? (
-          <div className="bots-animated-cards">
-            {deck.map((el) => <img key={el.id} src={cardBack} alt={el.name} />)}
-          </div>
-        )
-        : ''} */}
     </div>
   );
 }
